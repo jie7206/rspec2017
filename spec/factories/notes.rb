6ca04cc74335f5608f72b5ec6e5a4c00a3bd93bb
jie7206 <1524556052@qq.com>
2019-02-10 16:13:05 +0800

    finish chapter 4

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :note do
-    
+    message "My test note."
+    association :project
+    user {project.owner}
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,17 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:owner] do
     first_name "Michael"
     last_name "Lin"
-    sequence :email { |n| "jie#{n}@qq.com" }
+    sequence(:email) { |n| "jie#{n}@qq.com" }
     password "123456"
+
+    trait :shijie do
+      first_name "ShiJie"
+      last_name "Lin"
+      email "lin@qq.com"
+    end
+    trait :with_project do
+      after(:create) {|user| create(:project, name:"Test Project", owner: user)}
+    end
   end
 end
